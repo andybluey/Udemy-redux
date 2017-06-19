61e865278d97c8a93dbeeb3656b3a9443eb218a1
Andrew Knight <andrewjaknight@gmail.com>
2017-06-19 20:12:07 +1000

    First commit

---
+++ middlewares/src/middlewares/async.js
@@ -0,0 +1,16 @@
+export default function({ dispatch }) {
+  return next => action => {
+    // If the action doesn't have payload or the payload doesn't have
+    // a .then property, we don't care about it
+    if (!action.payload || !action.payload.then) {
+      return next(action);
+    }
+
+    // make sure the action's promise resolves
+    action.payload.then(function(response) {
+      // create a new action with the old type, but replace the promise with the response data
+      const newAction = { ...action, payload: response };
+      dispatch(newAction);
+    });
+  };
+}
